09d5938c2eb3d5a9154668e327f7cc48d0dfa222
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2022-09-30 11:31:32 +0200

    respect options

--- src/Traits/HasAvatarUrl.php
+++ src/Traits/HasAvatarUrl.php
@@ -10,7 +10,13 @@ trait HasAvatarUrl
     public function avatarUrl(): Attribute
     {
         return new Attribute(
-            get: fn () => (new FilamentBoringAvatarsServiceProvider())->get($this)
-        );
+			get: fn () => app()->make(BoringAvatars::class)->get(
+				$this->name,
+				config('filament-boring-avatars.variant', 'marble'),
+				config('filament-boring-avatars.size', 40),
+				config('filament-boring-avatars.colors', []),
+				config('filament-boring-avatars.url', 'https://source.boringavatars.com')
+			)
+		);
     }
 }
